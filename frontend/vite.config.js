06a48a003ae905d54b64397c125d369d5b6c0b11
Ketan <rajputketan160@gmail.com>
2025-05-22 01:21:03 -0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Fashion-Ecommerce-Website/',  // <-- यहाँ अपना repo नाम डालें
   plugins: [react()],
 })
